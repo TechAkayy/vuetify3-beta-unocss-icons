a15134d3510f3e4490b80b00e24fcdf2f31aab5b
akkayy <ahmed.kaja@outlook.com>
2022-05-20 23:05:04 +1000

    Added iconify mdi json

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
 	<v-app>
 		<v-main>
-			<v-alert icon="i-uil-shopping-cart">I'm an alert</v-alert>
+			<v-alert icon="i-uil-shopping-cart">I'm a shopping cart</v-alert>
+			<v-alert icon="i-mdi-lightning-bolt">I'm a lightning bolt</v-alert>
 		</v-main>
 	</v-app>
 </template>
