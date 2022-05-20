b7e453ec1455f1f5729417ff6b196abb40838ce5
akkayy <ahmed.kaja@outlook.com>
2022-05-20 23:19:49 +1000

    Minor update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
 	<v-app>
 		<v-main>
-			<v-alert icon="i-uil-shopping-cart">I'm a shopping cart</v-alert>
-			<v-alert icon="i-mdi-lightning-bolt">I'm a lightning bolt</v-alert>
-			<v-alert icon="i-logos-vue">I'm the Vue.js</v-alert>
-			<v-icon icon="i-mdi-home"></v-icon>
+			<v-alert icon="i-uil:shopping-cart">I'm a shopping cart</v-alert>
+			<v-alert icon="i-mdi:lightning-bolt">I'm a lightning bolt</v-alert>
+			<v-alert icon="i-logos:vue">I'm the Vue.js</v-alert>
+			<v-icon icon="i-mdi:home"></v-icon>
 		</v-main>
 	</v-app>
 </template>
