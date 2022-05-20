aaa9979c7e8830ded9473084ea26d7c923d25053
akkayy <ahmed.kaja@outlook.com>
2022-05-20 23:08:21 +1000

    Added iconify logos json

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
 		<v-main>
 			<v-alert icon="i-uil-shopping-cart">I'm a shopping cart</v-alert>
 			<v-alert icon="i-mdi-lightning-bolt">I'm a lightning bolt</v-alert>
+			<v-alert icon="i-logos-vue">I'm the Vue.js</v-alert>
+			<v-icon icon="i-mdi-home"></v-icon>
 		</v-main>
 	</v-app>
 </template>
